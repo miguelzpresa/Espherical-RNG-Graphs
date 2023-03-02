3dcec2a537b9337eae8798d5e7c7ca6a3e970f61
mzamorano <mikezpresa@gmail.com>
2023-03-01 18:55:36 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Numerical & Statistical Analysis about RNGs(Relative Neighbor Graphs) living on a spherical surface',
+    author='Dr.Daniele Colosi & Miguel Zamorano',
+    license='BSD-3',
+)
